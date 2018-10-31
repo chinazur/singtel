91961998c3ccc0080375b74b8f6d3b927ed4bfe3
Tang Ke <tangke850114@gmail.com>
2018-10-31 17:23:18 +0800

    Butterfly model

---
+++ src/main/java/com/singtel/demo/animal/others/Butterfly.java
@@ -0,0 +1,13 @@
+package com.singtel.demo.animal.others;
+
+import com.singtel.demo.animal.Animal;
+import com.singtel.demo.animal.interfaces.Flyable;
+
+public class Butterfly extends Animal implements Flyable {
+
+	@Override
+	public void fly() {
+
+	}
+
+}
